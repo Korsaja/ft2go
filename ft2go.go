c25e066064f6bb937b1d0941125e2b11f5245c2d
supreme <supreme@localhost.localdomain>
2020-10-12 14:19:56 +0400

    fix and update

--- ft2go.go
+++ ft2go.go
@@ -19,7 +19,9 @@ var usage = func() {
 	fmt.Println("usage: ./ft2go -path test/ " +
 		"-ex router1  -nf vlan10," +
 		" -conf config.toml -w 10 -pr=true" +
-		" -csv=true -emails test@email.com")
+		" -csv=true -emails test@email.com" +
+		"ex: ./bot -path var/log/netflow -ex br0  -nf satys," +
+		"-w 8 -pr=true -csv=true -emails username@mails.ru")
 }
 
 func SliceFilter(exAddr []net.IP, clients *Clients, batch []*ftrecord, numCPU int) {
@@ -135,7 +137,6 @@ func DataInfo(conf *Config, exAdders, filters, dir string) (ex []net.IP, c *Clie
 							}
 						}
 					}
-
 					clients = append(clients, c)
 				}
 			}
@@ -151,13 +152,16 @@ func main() {
 	dirFiles := flag.String("path", "", "file directory")
 	emails := flag.String("emails", "", "emails with sep=,")
 	subject := flag.String("subj", "Netflow", "subject for mail")
-	numCPU := flag.Int("w", 1, "workers")
+	numCPU := flag.Int("w", 8, "workers")
 	print := flag.Bool("pr", false, "print table")
 	csvFile := flag.Bool("csv", false, "csv file")
 	help := flag.String("h","","help")
 	flag.Parse()
 	flag.Usage = usage
-	if len(os.Args) < 2 || len(*help) != 0{
+	if len(os.Args) < 2 ||
+		len(*help) != 0 ||
+		len(*filters) == 0 ||
+		len(*exAdders) == 0{
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -195,6 +199,7 @@ func main() {
 	if *print {
 		clients.Report(os.Stdout)
 	}
+
 	if *csvFile {
 		err = clients.WriteCSV()
 		if err != nil {
@@ -205,26 +210,30 @@ func main() {
 	if len(*emails) != 0 {
 		mails := strings.Split(*emails, ",")
 		d := mail.NewDialer(conf.SMTP.Server, conf.SMTP.Port, conf.SMTP.Mail,conf.SMTP.Pass)
-		m := mail.NewMessage()
-		m.SetHeader("From",conf.SMTP.Mail)
-		m.SetHeader("To",mails...)
-		m.SetHeader("Subject",*subject)
-		m.SetBodyWriter("text/plain", func(writer io.Writer) error {
-			clients.Report(writer)
-			return nil
-		})
-		d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-		if *csvFile {
-			f, err := os.Open(clients.GetNames())
-			if err != nil {
+		for _, xMail := range mails{
+			m := mail.NewMessage()
+			m.SetHeader("From",conf.SMTP.Mail)
+			m.SetHeader("To",xMail)
+			m.SetHeader("Subject",*subject)
+			m.SetBodyWriter("text/plain", func(writer io.Writer) error {
+				clients.Report(writer)
+				return nil
+			})
+			d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+			if *csvFile {
+				f, err := os.Open(clients.GetNames())
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+				}
+				defer f.Close()
+				m.Attach(clients.GetNames())
+			}
+			if err := d.DialAndSend(m); err != nil {
 				fmt.Fprintln(os.Stderr, err)
 			}
-			defer f.Close()
-			m.Attach(clients.GetNames())
+			fmt.Fprintf(os.Stdout,"Send mail to :: %s :: Done\n",xMail)
 		}
-		if err := d.DialAndSend(m); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
+
 	}
 	fmt.Println("Done.")
 }
